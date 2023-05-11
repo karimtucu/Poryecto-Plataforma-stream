feedb862b948f7d070ee9789d759b9a962620f9c
Santiago Mamani <santiagomamani2004@gmail.com>
2023-05-11 15:06:07 -0300

    ultimos retoques

--- proyecto-n2/main.js
+++ proyecto-n2/main.js
@@ -212,3 +212,15 @@ modal_container.classList.add('show');
 close.addEventListener('click', () => {
 modal_container.classList.remove('show');
 });
+///? ---------mandar gmail con contacto-------
+const $form = document.querySelector('#form')
+    const $buttonMailto = document.querySelector('#trucazo')
+
+    $form.addEventListener('submit', handleSubmit)
+
+    function handleSubmit(event) {
+      event.preventDefault()
+      const form = new FormData(this)
+      $buttonMailto.setAttribute('href', `mailto:me@leonidasesteban.com?subject=nombre ${form.get('name')}  correo ${form.get('email')}&body=${form.get('message')}`)
+      $buttonMailto.click()
+    }
